6c022b269567bc6bf4945aea674c32483d4d365e
Bartlomiej Mont <bartlomiejmont@gmail.com>
2019-04-13 22:49:53 +0200

    adding items to listview

--- app/src/main/java/com/example/narcfactsknowyoursubstance/ListActivity.java
+++ app/src/main/java/com/example/narcfactsknowyoursubstance/ListActivity.java
@@ -3,20 +3,48 @@ package com.example.narcfactsknowyoursubstance;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
+        String drugs [] = getResources().getStringArray(R.array.support_issues_array);
 
 
-        // Capture the layout's TextView and set the string as its text
+        lvItems = (ListView) findViewById(R.id.drugs_listview);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+
+        fillList(drugs);
+
     }
+
+    private void fillList (String[] a){
+
+        for(String x:a){
+            items.add(x);
+        }
+
+    }
+
+
+
 
 }
