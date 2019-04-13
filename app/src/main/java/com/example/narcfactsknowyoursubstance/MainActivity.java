7a11eef39bba32cbff1d281a5309872a716be614
bartlomiejmont <bartlomiejmont@gmail.com>
2019-04-13 18:15:26 +0200

    ...

---
+++ app/src/main/java/com/example/narcfactsknowyoursubstance/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.narcfactsknowyoursubstance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+    public void showDonutOrder(View view) {
+        displayToast(getString(R.string.donut_order_message));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
